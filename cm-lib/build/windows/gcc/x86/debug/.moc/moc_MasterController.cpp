bc41b47b5b03a85271e2b95afff49806a9177d20
Alexander Kushnirenko <kushnirenko.alexander784@gmail.com>
2020-01-25 19:18:39 +0200

    New build

--- cm-lib/build/windows/gcc/x86/debug/.moc/moc_MasterController.cpp
+++ cm-lib/build/windows/gcc/x86/debug/.moc/moc_MasterController.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_cm__controllers__MasterController_t {
-    QByteArrayData data[4];
-    char stringdata0[115];
+    QByteArrayData data[6];
+    char stringdata0[172];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,12 +34,16 @@ static const qt_meta_stringdata_cm__controllers__MasterController_t qt_meta_stri
 QT_MOC_LITERAL(0, 0, 33), // "cm::controllers::MasterContro..."
 QT_MOC_LITERAL(1, 34, 17), // "ui_welcomeMessage"
 QT_MOC_LITERAL(2, 52, 23), // "ui_navigationController"
-QT_MOC_LITERAL(3, 76, 38) // "cm::controllers::NavigationCo..."
+QT_MOC_LITERAL(3, 76, 38), // "cm::controllers::NavigationCo..."
+QT_MOC_LITERAL(4, 115, 20), // "ui_commandController"
+QT_MOC_LITERAL(5, 136, 35) // "cm::controllers::CommandContr..."
 
     },
     "cm::controllers::MasterController\0"
     "ui_welcomeMessage\0ui_navigationController\0"
-    "cm::controllers::NavigationController*"
+    "cm::controllers::NavigationController*\0"
+    "ui_commandController\0"
+    "cm::controllers::CommandController*"
 };
 #undef QT_MOC_LITERAL
 
@@ -50,7 +54,7 @@ static const uint qt_meta_data_cm__controllers__MasterController[] = {
        0,       // classname
        0,    0, // classinfo
        0,    0, // methods
-       2,   14, // properties
+       3,   14, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
@@ -59,6 +63,7 @@ static const uint qt_meta_data_cm__controllers__MasterController[] = {
  // properties: name, type, flags
        1, QMetaType::QString, 0x00095401,
        2, 0x80000000 | 3, 0x00095009,
+       4, 0x80000000 | 5, 0x00095009,
 
        0        // eod
 };
@@ -68,6 +73,8 @@ void cm::controllers::MasterController::qt_static_metacall(QObject *_o, QMetaObj
     if (_c == QMetaObject::RegisterPropertyMetaType) {
         switch (_id) {
         default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 2:
+            *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< cm::controllers::CommandController* >(); break;
         case 1:
             *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< cm::controllers::NavigationController* >(); break;
         }
@@ -81,6 +88,7 @@ void cm::controllers::MasterController::qt_static_metacall(QObject *_o, QMetaObj
         switch (_id) {
         case 0: *reinterpret_cast< QString*>(_v) = _t->welcomeMessage(); break;
         case 1: *reinterpret_cast< cm::controllers::NavigationController**>(_v) = _t->navigationController(); break;
+        case 2: *reinterpret_cast< cm::controllers::CommandController**>(_v) = _t->commandController(); break;
         default: break;
         }
     } else if (_c == QMetaObject::WriteProperty) {
@@ -123,17 +131,17 @@ int cm::controllers::MasterController::qt_metacall(QMetaObject::Call _c, int _id
    if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
             || _c == QMetaObject::ResetProperty || _c == QMetaObject::RegisterPropertyMetaType) {
         qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::QueryPropertyDesignable) {
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::QueryPropertyScriptable) {
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::QueryPropertyStored) {
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::QueryPropertyEditable) {
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::QueryPropertyUser) {
-        _id -= 2;
+        _id -= 3;
     }
 #endif // QT_NO_PROPERTIES
     return _id;
